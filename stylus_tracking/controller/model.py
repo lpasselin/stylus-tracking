ddcaab9a0bd0564858718d2978e48b27c2012855
genou <gegilbert@hotmail.com>
2018-12-19 17:21:23 -0500

    quick fix

--- stylus_tracking/controller/model.py
+++ stylus_tracking/controller/model.py
@@ -1,7 +1,3 @@
-def __from_homogeneous_to_real(point):
-    return [point[0] / point[3], point[1] / point[3], point[2] / point[3]]
-
-
 class AppModel:
     def __init__(self):
         self.current_frame = None
@@ -12,7 +8,10 @@ class AppModel:
         self.z = []
 
     def add_point(self, point):
-        real = __from_homogeneous_to_real(point)
+        real = self.__from_homogeneous_to_real(point)
         self.x.append(real[0])
         self.y.append(real[1])
         self.z.append(real[2])
+
+    def __from_homogeneous_to_real(self, point):
+        return [point[0] / point[3], point[1] / point[3], point[2] / point[3]]
