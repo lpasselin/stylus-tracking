51e0da3ec2a51f729a439cb7cb9421ef27220f93
genou <gegilbert@hotmail.com>
2018-12-19 18:10:12 -0500

    add axis

--- stylus_tracking/ui/graph.py
+++ stylus_tracking/ui/graph.py
@@ -10,10 +10,16 @@ class Graph:
         fig = Figure(figsize=(width, height))
         self.graph_canvas = FigureCanvasTkAgg(fig, master=master)
         self.ax = Axes3D(fig)
+        self.ax.set_xlabel("x")
+        self.ax.set_ylabel("y")
+        self.ax.set_zlabel("z")
         self.ax.mouse_init()
         self.graph_canvas.get_tk_widget().grid(row=1, column=2)
 
     def update(self, x, y, z):
         self.ax.clear()
+        self.ax.set_xlabel("X axis")
+        self.ax.set_ylabel("Y axis")
+        self.ax.set_zlabel("Z axis")
         self.ax.scatter(x, y, z)
         self.graph_canvas.draw()
