8b6968de843d1435c5ecbde2f7208fad2327dc9d
lpasselin <louisphilippeasselin@gmail.com>
2018-12-20 00:16:24 -0500

    durty fucking cheat to fix line problem

--- stylus_tracking/controller/model.py
+++ stylus_tracking/controller/model.py
@@ -2,42 +2,47 @@ import numpy as np
 
 
 def remove_np_duplicates(data):
-  # Perform lex sort and get sorted data
-  sorted_idx = np.lexsort(data.T)
-  sorted_data =  data[sorted_idx,:]
+    # Perform lex sort and get sorted data
+    sorted_idx = np.lexsort(data.T)
+    sorted_data = data[sorted_idx, :]
 
-  # Get unique row mask
-  row_mask = np.append([True],np.any(np.diff(sorted_data,axis=0),1))
+    # Get unique row mask
+    row_mask = np.append([True], np.any(np.diff(sorted_data, axis=0), 1))
 
-  # Get unique rows
-  out = sorted_data[row_mask]
-  return out
+    # Get unique rows
+    out = sorted_data[row_mask]
+    return out
 
-def get_grid_cells_btw(p1,p2):
-  x1,y1 = p1
-  x2,y2 = p2
-  dx = x2-x1
-  dy = y2-y1
 
-  if x1 == x2 and y2 == y1:
-      return []
-  if dx == 0: # will divide by dx later, this will cause err. Catch this case up here
-    step = np.sign(dy)
-    ys = np.arange(0,dy+step,step)
-    xs = np.repeat(x1, ys.shape[0])
-  else:
-    m = dy/(dx+0.0)
-    b = y1 - m * x1
+def get_grid_cells_btw(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    dx = x2 - x1
+    dy = y2 - y1
 
-    step = 1.0/(max(abs(dx),abs(dy)))
-    xs = np.arange(x1, x2, step * np.sign(x2-x1))
-    ys = xs * m + b
+    if x1 == x2 and y2 == y1:
+        return []
+    if dx == 0:  # will divide by dx later, this will cause err. Catch this case up here
+        step = np.sign(dy)
+        ys = np.arange(0, dy + step, step)
+        xs = np.repeat(x1, ys.shape[0])
+    else:
+        m = dy / (dx + 0.0)
+        b = y1 - m * x1
 
-  xs = np.rint(xs)
-  ys = np.rint(ys)
-  pts = np.column_stack((xs,ys))
-  pts = remove_np_duplicates(pts)
-  return pts.astype(int)
+        step = 1.0 / (max(abs(dx), abs(dy)))
+        xs = np.arange(x1, x2, step * np.sign(x2 - x1))
+        ys = xs * m + b
+
+    xs = np.rint(xs)
+    ys = np.rint(ys)
+    pts = np.column_stack((xs, ys))
+
+    if x1 == x2:
+        pts[:,1] += y1
+
+    return pts.astype(int)
+
 
 class AppModel:
     SCALER = 4
@@ -98,7 +103,6 @@ class AppModel:
                 self.drawing[x, y, :] = iz, iz, iz
         self.last_x = ix
         self.last_y = iy
-
 
     def __from_homogeneous_to_real(self, point):
         return [point[0] / point[3], point[1] / point[3], point[2] / point[3]]
