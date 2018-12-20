641a9ca583a429047e45d995ba06529ce773adc4
Etienne Dubeau <etienne.dubeau.1@ulaval.ca>
2018-12-19 23:45:05 -0500

    Add line drawing

--- stylus_tracking/controller/model.py
+++ stylus_tracking/controller/model.py
@@ -1,6 +1,42 @@
 import numpy as np
 
 
+def remove_np_duplicates(data):
+  # Perform lex sort and get sorted data
+  sorted_idx = np.lexsort(data.T)
+  sorted_data =  data[sorted_idx,:]
+
+  # Get unique row mask
+  row_mask = np.append([True],np.any(np.diff(sorted_data,axis=0),1))
+
+  # Get unique rows
+  out = sorted_data[row_mask]
+  return out
+
+def get_grid_cells_btw(p1,p2):
+  x1,y1 = p1
+  x2,y2 = p2
+  dx = x2-x1
+  dy = y2-y1
+
+  if dx == 0: # will divide by dx later, this will cause err. Catch this case up here
+    step = np.sign(dy)
+    ys = np.arange(0,dy+step,step)
+    xs = np.repeat(x1, ys.shape[0])
+  else:
+    m = dy/(dx+0.0)
+    b = y1 - m * x1
+
+    step = 1.0/(max(abs(dx),abs(dy)))
+    xs = np.arange(x1, x2, step * np.sign(x2-x1))
+    ys = xs * m + b
+
+  xs = np.rint(xs)
+  ys = np.rint(ys)
+  pts = np.column_stack((xs,ys))
+  pts = remove_np_duplicates(pts)
+  return pts.astype(int)
+
 class AppModel:
     SCALER = 4
     X = 103 * SCALER
@@ -17,6 +53,9 @@ class AppModel:
         self.y = []
         self.z = []
         self.drawing = np.zeros([self.X * 2 + 2, self.Y * 2 + 2, 3], dtype=np.uint8)
+        self.last_x = None
+        self.last_y = None
+        self.last_z = None
 
     def add_point(self, point):
         real = self.__from_homogeneous_to_real(point)
@@ -49,6 +88,15 @@ class AppModel:
         self.drawing[ix, iy - 1, :] = iz, iz, iz
         self.drawing[ix + 1, iy, :] = iz, iz, iz
         self.drawing[ix - 1, iy, :] = iz, iz, iz
+
+        if self.last_x:
+            for i in get_grid_cells_btw((self.last_x, self.last_y), (ix, iy)):
+                x = i[0]
+                y = i[1]
+                self.drawing[x, y, :] = iz, iz, iz
+        self.last_x = ix
+        self.last_y = iy
+
 
     def __from_homogeneous_to_real(self, point):
         return [point[0] / point[3], point[1] / point[3], point[2] / point[3]]
